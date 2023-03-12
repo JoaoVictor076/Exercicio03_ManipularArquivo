c25b4d05bb8f0681d8e99ec5126e621daea6942c
JoaoVictor076 <115574299+JoaoVictor076@users.noreply.github.com>
2023-03-12 19:09:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ManipularArquivo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class program
+{
+    public static void Main()
+    {
+        manipulacaoArquivo manipulador = new manipulacaoArquivo();
+        manipulador.Conteudo = "joao victor";
+        manipulador.EnderecoNomeArquivo = @"C:\Users\joaok\Desktop\arquivo\manipular.txt";
+        if (manipulador.SalvarDadosEArquivo())
+        {
+            Console.WriteLine("arquivo salvo com sucesso!");
+        }
+        Console.WriteLine(manipulador.PesquisarConteudoArquivo(manipulador.EnderecoNomeArquivo));
+    }
+}
---
+++ manipulacaoArquivo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipularArquivo
+{
+    public class manipulacaoArquivo
+    {
+        public string Arquivo;
+        public string DiretorioArquivo;
+
+        public bool SalvarDadosEArquivo()
+        {
+            try
+            {
+                if (!File.Exists(DiretorioArquivo))
+                    File.Create(DiretorioArquivo).Close();
+
+                StreamWriter write = new StreamWriter(DiretorioArquivo);
+                write.Write(Arquivo);
+                write.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        public string PesquisarConteudoArquivo(string diretorio)
+        {
+            if (!File.Exists(diretorio))
+                return "";
+
+            StreamReader sr = new StreamReader(diretorio);
+            string arquivo = sr.ReadToEnd();
+            sr.Close();
+            return arquivo;
+
+        }
+
+
+    }
+}
+
